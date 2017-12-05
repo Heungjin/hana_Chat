d82b5e7aed26cbc99456849d87603cf2abdc81e6
HJJ <orolgo@g.ucla.edu>
2017-12-05 11:45:27 +0900

    6th

--- answer/views.py
+++ answer/views.py
@@ -6,7 +6,7 @@ import json
 def keyboard(request):
     return JsonResponse({
         'type': 'buttons',
-        'buttons': ['시작하기', '도움말'] # start button for user
+        'buttons': ['1', '2'] # start button for user
     })
 
 def message(request):
@@ -23,36 +23,35 @@ def message(request):
     if start:
         return JsonResponse({
             'message': {
-                'text': "얼마고를 시작합니다. 핸드폰 기종을 선택하여 주세요!",
+                'text': "1",
             },
             'keyboard': {
                 'type': 'buttons',
-                'buttons': ['시작하기', '도움말']
+                'buttons': ['1', '2']
             },
         })
 
     elif help:
         return JsonResponse({
             'message': {
-                'text': "하월은 韓World의 약자로 한국에서 세계로 나가고자 하는 의미가 담겨있습니다. " +
-                        "하월은 사회초년생의 최적의 전세 상품을 찾아드리고 그와 같은 월세가격을 측정합니다.",
+                'text': "1",
             },
             'keyboard': {
                 'type': 'buttons',
-                'buttons': ['시작하기', '도움말']
+                'buttons': ['1', '2']
             },
         })
 
 # user input is start button check
 def check_is_start(str):
-    if str == "시작하기":
+    if str == "1":
         return True
     else:
         return False
 
 # user input is help button check
 def check_is_help(str):
-    if str == "도움말":
+    if str == "2":
         return True
     else:
         return False
