8a66f8d7822bcf167971580c7b4f522c69419a2a
HJJ <orolgo@g.ucla.edu>
2017-12-09 13:55:23 +0900

    sat 11:40

--- answer/views.py
+++ answer/views.py
@@ -97,7 +97,7 @@ def message(request):
         loanGoods = LoanGoods.objects.get(loan_good_name=(return_str).encode('utf-8'))
         loanGoods.chat_recommend = loanGoods.chat_recommend + 1
         loanGoods.save()
-        LoanGoods.objects.values_list('loan_good_name', flat=True).order_by('-chat_recommend').save()
+        (LoanGoods.objects.values_list('loan_good_name', flat=True).order_by('-chat_recommend')).save()
         # loanGoodsDesc = list(LoanGoods.objects.filter(chatbot_description=loanGoods))
 
         # list(LoanGoods.objects.values_list('loan_good_name', flat=True).filter(loan_repayment=1))[0]
