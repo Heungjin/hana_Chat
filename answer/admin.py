0bc4042d6d9f56c9873520c4894ee61274455096
heungjin jeon <orolgo@g.ucla.edu>
2017-12-13 14:29:30 +0900

    hello

--- answer/admin.py
+++ answer/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import LoanGoods # 모델에서 LoanGoods를 불러온다
 from .models import CustomerInfo # 모델에서 CustomerInfo를 불러온다
 from .models import Banks # 모델에서 CustomerInfo를 불러온다
+from .models import StatisticAge # 모델에서 CustomerInfo를 불러온다
 
 
 
