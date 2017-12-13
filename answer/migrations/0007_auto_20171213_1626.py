9fd6f0c87f4c57aaa561dff41be46ec4a76c7985
heungjin jeon <orolgo@g.ucla.edu>
2017-12-13 16:46:25 +0900

    'nn'

--- answer/migrations/0007_auto_20171213_1626.py
+++ answer/migrations/0007_auto_20171213_1626.py
@@ -20,5 +20,17 @@ class Migration(migrations.Migration):
         migrations.AlterModelOptions(
             name='statisticlist',
             options={},
-        )
+        ),
+        migrations.AddField(
+            model_name='statisticage',
+            name='id',
+            field=models.AutoField(auto_created=None, default="", primary_key=True, serialize=False, verbose_name='ID'),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='statisticlist',
+            name='id',
+            field=models.AutoField(auto_created=None, default=type, primary_key=True, serialize=False, verbose_name='ID'),
+            preserve_default=False,
+        ),
     ]
