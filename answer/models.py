2731ee5e29e1f1fcf24044d7bae9b248b7903788
heungjin jeon <orolgo@g.ucla.edu>
2017-12-13 16:42:41 +0900

    'nn'

--- answer/models.py
+++ answer/models.py
@@ -10,7 +10,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 class StatisticAge(models.Model):
-    type = models.CharField(max_length=255, blank=True, null=True)
+    type = models.CharField(max_length=255, blank=True, null=True, primary_key=True)
     m0_20 = models.IntegerField(blank=True, null=True)
     m20_30 = models.IntegerField(blank=True, null=True)
     m30_40 = models.IntegerField(blank=True, null=True)
@@ -19,11 +19,12 @@ class StatisticAge(models.Model):
     m60_0 = models.IntegerField(blank=True, null=True)
 
     class Meta:
+        managed = False
         db_table = 'statistic_age'
 
 
 class StatisticList(models.Model):
-    type = models.CharField(max_length=255, blank=True, null=True)
+    type = models.CharField(max_length=255, blank=True, null=True, primary_key=True)
     m1_loan_good_num = models.IntegerField(blank=True, null=True)
     m1_num = models.IntegerField(blank=True, null=True)
     m2_loan_good_num = models.IntegerField(blank=True, null=True)
@@ -33,6 +34,7 @@ class StatisticList(models.Model):
     total = models.IntegerField(blank=True, null=True)
 
     class Meta:
+        managed = False
         db_table = 'statistic_list'
 
 
