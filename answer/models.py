05b0f04a4fd65c0368bffb035f9f4ff32f4a537d
heungjin jeon <orolgo@g.ucla.edu>
2017-12-13 17:24:23 +0900

    '123'

--- answer/models.py
+++ answer/models.py
@@ -10,7 +10,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 class StatisticAge2(models.Model):
-    type = models.CharField(max_length=255, blank=True, primary_key=True)
+    type = models.CharField(max_length=255, blank=True, primary_key=False)
     m0_20 = models.IntegerField(blank=True, null=True)
     m20_30 = models.IntegerField(blank=True, null=True)
     m30_40 = models.IntegerField(blank=True, null=True)
@@ -24,7 +24,7 @@ class StatisticAge2(models.Model):
 
 
 class StatisticList2(models.Model):
-    type = models.CharField(max_length=255, blank=True, primary_key=True)
+    type = models.CharField(max_length=255, blank=True, primary_key=False)
     m1_loan_good_num = models.IntegerField(blank=True, null=True)
     m1_num = models.IntegerField(blank=True, null=True)
     m2_loan_good_num = models.IntegerField(blank=True, null=True)
