06c81649693bfad1df4996b0ec861fc781a1ad63
heungjin jeon <orolgo@g.ucla.edu>
2017-12-13 19:11:45 +0900

    '123'

--- answer/views.py
+++ answer/views.py
@@ -105,7 +105,7 @@ def message(request):
             },
             'keyboard': {
                 'type': 'buttons',
-                'buttons': StatAgeList
+                'buttons': stat_list
             },
         })
 
