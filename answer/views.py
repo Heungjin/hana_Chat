2a1dfeb98a6a53522075ea68027b38547023d13b
hjj <orolgo@g.ucla.edu>
2017-12-14 23:41:48 +0900

    '123';

--- answer/views.py
+++ answer/views.py
@@ -105,7 +105,7 @@ def message(request):
     test_ranking = list(LoanGoods.objects.values_list('loan_good_name', flat=True).order_by('-chat_recommend'))
     test_ranking_Str = "\n * ".join(test_ranking).encode('utf8')
 
-    user_check = list(User.objects.values_list('user_key', flat=True).filter(user_key=user_key)).count()
+    user_check = len(list(User.objects.values_list('user_key', flat=True).filter(user_key=user_key)))
 
     # if start button check
     print(return_str)
